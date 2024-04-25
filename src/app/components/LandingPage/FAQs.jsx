3d7ff4198e60844e1daaf8cf0962f33ff937537b
placeparks <placeparks@gmail.com>
2024-04-25 22:30:20 +0500

    changes made in bg

--- src/app/components/LandingPage/FAQs.jsx
+++ src/app/components/LandingPage/FAQs.jsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 const AccordionItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,20 +17,22 @@ const AccordionItem = ({ question, answer }) => {
         >
           {question}
           <svg
-            className={`w-6 h-6 transform transition-transform ${isOpen ? 'rotate-180' : ''}`}
+            className={`w-6 h-6 transform transition-transform ${
+              isOpen ? "rotate-180" : ""
+            }`}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
-            <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
           </svg>
         </button>
       </h2>
-      <div
-        className={`p-5 ${isOpen ? 'block' : 'hidden'}`}
-      >
-        {answer}
-      </div>
+      <div className={`p-5 ${isOpen ? "block" : "hidden"}`}>{answer}</div>
     </>
   );
 };
@@ -38,37 +40,42 @@ const AccordionItem = ({ question, answer }) => {
 const Questions = () => {
   return (
     <div className="container my-24 mx-auto px-6 xl:px-24">
-          <section className="mb-32">
-              <div className='flex flex-col justify-center items-center'>
-        <h2 className="mb-6 text-3xl font-bold">Frequently asked questions</h2>
-       <p className='text-gray-400 text-center w-full mb-4 md:mb-12 md:w-3/4'>Our FAQ area is the best place to look to find answers to your questions. Our community and support team constantly updates the questions and answers.</p>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  <AccordionItem
+      <section className="mb-32">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="mb-6 text-3xl font-bold">
+            Frequently asked questions
+          </h2>
+          <p className="text-gray-400 text-center w-full mb-4 md:mb-12 md:w-3/4">
+            Our FAQ area is the best place to look to find answers to your
+            questions. Our community and support team constantly updates the
+            questions and answers.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <AccordionItem
             question="How does your SaaS Product work?"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-                       <AccordionItem
+          />
+          <AccordionItem
             question=" Can I customize my preferences?"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-                       <AccordionItem
+          />
+          <AccordionItem
             question="Are you GDPR compliant?"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-                         <AccordionItem
+          />
+          <AccordionItem
             question="Do you offer unlimited updates?"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-                                   <AccordionItem
+          />
+          <AccordionItem
             question="Can I update my payment method"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-                                   <AccordionItem
+          />
+          <AccordionItem
             question="How to restore my account?"
             answer="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. "
-                  />
-         
+          />
         </div>
       </section>
     </div>
--- src/app/components/LandingPage/HomeLayout.jsx
+++ src/app/components/LandingPage/HomeLayout.jsx
@@ -9,27 +9,32 @@ import Portfolio from "./Portfolio";
 import Questions from "./FAQs";
 
 const HomeLayout = () => {
-
   return (
     <>
-    <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 flex flex-col items-center justify-center font-sans text-bb md:pt-32 -mt-14 md:mt-0 pointer-events-none text-center">
-      <Image src='/assets/WELCOME.png' height={300} width={300} alt=""  />
-        <h1 className="font-bold md:text-7xl text-3xl md:pr-20 md:pl-20 p-8 md:leading-[6rem]">Discover, Experiment, Buy & Sell Pretrained AI Models</h1>
-        <p className="md:text-lg text-md font-normal pl-4 pr-4 md:pl-8 md:pr-8 md:pl-80 md:pr-80 mb-8" >A platform that allows individuals and organisations to buy and sell pretrained AI models and services securely and transparently using blockchain technology</p>
+      <BackgroundGradientAnimation>
+        <div className="absolute z-50 inset-0 flex flex-col items-center justify-center font-sans text-bb md:pt-32 -mt-14 md:mt-0 pointer-events-none text-center">
+          <Image src="/assets/WELCOME.png" height={300} width={300} alt="" />
+          <h1 className="font-bold md:text-7xl text-3xl md:pr-20 md:pl-20 p-8 md:leading-[6rem]">
+            Discover, Experiment, Buy & Sell Pretrained AI Models
+          </h1>
+          <p className="md:text-lg text-md font-normal pl-4 pr-4 md:pl-8 md:pr-8 md:pl-80 md:pr-80 mb-8">
+            A platform that allows individuals and organisations to buy and sell
+            pretrained AI models and services securely and transparently using
+            blockchain technology
+          </p>
           <Button title="Explore" />
           <footer className="md:pt-40 pt-14 pl-8 pr-8 md:pl-0 md:pr-0">
-          <Image src='/assets/brands.png' width={1200} height={700} alt=""/>
-        </footer>
+            <Image src="/assets/brands.png" width={1200} height={700} alt="" />
+          </footer>
         </div>
-    </BackgroundGradientAnimation>
-    <Features/>
-    <AIPage/>
-    <Cards/>
+      </BackgroundGradientAnimation>
+      <Features />
+      <AIPage />
+      <Cards />
       <Portfolio />
-      <Questions/>
-      </>
-  )
-}
+      <Questions />
+    </>
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
